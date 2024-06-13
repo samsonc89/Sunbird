550e28367bd8b880a88b88f1f3d0d9e2ef89cd84
Caleb Tseng <calebtseng07@gmail.com>
2024-06-13 03:05:11 -0700

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://ctseng07.github.io',
+  site: 'https://ctseng07.github.io',
   base: '/SunbirdSpeechTherapy',
-})
+  integrations: [tailwind()]
+});
