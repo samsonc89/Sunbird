4c88d92cf3374bd04d2c6dd852561de0c15a1878
Caleb Tseng <calebtseng07@gmail.com>
2024-07-03 03:56:35 -0700

    continue to work with service and philosophy section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import hero from "./images/hero.png";
 import philosophy from "./images/philosophy.png";
 import rana from "./images/rana.png";
+import vector from "./images/vector.png";
 
 export {
     hero,
     philosophy,
     rana,
+    vector
 }
