8d5001950c8a992689f207110ac09b4b3b402645
Caleb Tseng <calebtseng07@gmail.com>
2024-07-10 04:02:42 -0700

    Attempt to add carousel to testimonal card component

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,16 @@ import hero from "./images/hero.png";
 import philosophy from "./images/philosophy.png";
 import rana from "./images/rana.png";
 import vector from "./images/vector.png";
+import test1 from "./images/test1.png";
+import test2 from "./images/test2.png";
+import test3 from "./images/test3.png";
 
 export {
     hero,
     philosophy,
     rana,
-    vector
+    vector,
+    test1,
+    test2,
+    test3
 }
