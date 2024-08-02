83db2ec935edbe5cf59bb0f2104a99356137971a
Caleb Tseng <calebtseng07@gmail.com>
2024-08-02 05:07:30 -0700

    Work on testimonial page

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,10 @@ import test1 from "./images/test1.png";
 import test2 from "./images/test2.png";
 import test3 from "./images/test3.png";
 import calendly from "./images/calendly.png";
+import mh from "./images/MH.png";
+import kt from "./images/KT.png";
+import th from "./images/TH.png";
+import ds from "./images/DS.png";
 
 export {
     hero,
@@ -15,5 +19,9 @@ export {
     test1,
     test2,
     test3,
-    calendly
+    calendly,
+    mh,
+    kt,
+    th,
+    ds
 }
