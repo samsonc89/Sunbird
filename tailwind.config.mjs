13905e4806e18b51e4b6ef5b86434f9614114ac7
Caleb Tseng <calebtseng07@gmail.com>
2024-09-26 04:41:40 -0700

    Revert code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-	'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	'./node_modules/flowbite/**/*.js'
-];
-export const theme = {
-	extend: {},
-};
-export const plugins = [
-	require('flowbite/plugin')
-];
+module.exports = {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
