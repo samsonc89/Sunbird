c3aca5bf893970487d83774f79eaa936e8c62cd7
Caleb Tseng <calebtseng07@gmail.com>
2024-09-27 02:25:27 -0700

    clean code

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,6 @@ import hero from "./images/hero.png";
 import philosophy from "./images/philosophy.png";
 import rana from "./images/rana.jpg";
 import vector from "./images/vector.png";
-import test1 from "./images/test1.png";
-import test2 from "./images/test2.png";
-import test3 from "./images/test3.png";
 import calendly from "./images/calendly.png";
 import mh from "./images/MH.png";
 import kt from "./images/KT.jpg";
@@ -19,9 +16,6 @@ export {
   philosophy,
   rana,
   vector,
-  test1,
-  test2,
-  test3,
   calendly,
   mh,
   kt,
