c082a389394276a09b70bceb29f21dbd1a186d75
Samson Cheung <samsonc89@yahoo.com>
2024-12-23 08:47:13 -0800

    Rerun build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sunbirdspeechtherapy.com",
-  base: "/Sunbird",
+  base: "/",
   integrations: [tailwind(), react()],
 });
