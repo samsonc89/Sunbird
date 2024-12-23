c90aae17bad0f1dae99f79cde3ec97d00f7c9c9a
Samson Cheung <samsonc89@yahoo.com>
2024-12-23 09:09:53 -0800

    Update astro config to add cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sunbirdspeechtherapy.com",
   base: "/",
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: cloudflare()
 });
